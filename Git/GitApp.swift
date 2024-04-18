f5e93d6b03d22dea0895ff403ef338a143aae389
trudy <rlawldbs1237@gmail.com>
2024-04-18 20:14:41 +0900

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by 트루디 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
